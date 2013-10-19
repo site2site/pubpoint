133cbfa3712c598140cc2ebdec52197ab967e1b5
Travis Donia <tdonia@gmail.com>
2013-10-19 17:27:38 -0400

    testing

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var Spacebrew = require('./sb-1.3.0').Spacebrew,
     sb,
     config = require("./machine"),
     tumblr = require('tumblr.js');
-
+/*
 var files_location = "files/";
 var filepath = "./" + files_location;
 var hosted_path = "http://api.sitetosite.co/modules/file-server/" + files_location;
@@ -23,8 +23,10 @@ var tumblr_defaults = {
       type: 'photo',
       caption: 'Flickr', 
       source: 'http://farm9.staticflickr.com/8294/7733962082_11a1834801.jpg'
-    };
+    };*/
 
+
+console.dir(config);
 sb = new Spacebrew.Client( config.server, config.name, config.description );  // create spacebrew client object
 
 sb.addSubscribe("url", "string", ""); 
@@ -34,11 +36,11 @@ sb.onOpen = onOpen;
 function onStringMessage( name, value ){
       if(name == "url"){
         console.log('file url received: '+value);
-        tumblr_defaults["source"] = value;
-        tumblr_client.photo("pubpoint.tumblr.com", tumblr_defaults, function(err, json){
-          console.log(err);
-          console.log(json);
-        });
+        // tumblr_defaults["source"] = value;
+        // tumblr_client.photo("pubpoint.tumblr.com", tumblr_defaults, function(err, json){
+        //   console.log(err);
+        //   console.log(json);
+        // });
       }
 }
 
